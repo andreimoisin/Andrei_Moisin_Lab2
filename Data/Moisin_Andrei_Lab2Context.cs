42940475025fcbc22e73590e4c0a8fefb09877f1
andreimoisin <98484063+andreimoisin@users.noreply.github.com>
2022-11-01 23:11:31 +0200

    Finalizare tema laboratorul 2

--- Data/Moisin_Andrei_Lab2Context.cs
+++ Data/Moisin_Andrei_Lab2Context.cs
@@ -17,5 +17,7 @@ namespace Moisin_Andrei_Lab2.Data
         public DbSet<Moisin_Andrei_Lab2.Models.Book> Book { get; set; } = default!;
 
         public DbSet<Moisin_Andrei_Lab2.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Moisin_Andrei_Lab2.Models.Author> Author { get; set; }
     }
 }
