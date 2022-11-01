502679f46350586e2ed98dd4ab90bf16695ac962
andreimoisin <98484063+andreimoisin@users.noreply.github.com>
2022-11-01 19:56:36 +0200

    Add project files.

---
+++ Data/Moisin_Andrei_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Moisin_Andrei_Lab2.Models;
+
+namespace Moisin_Andrei_Lab2.Data
+{
+    public class Moisin_Andrei_Lab2Context : DbContext
+    {
+        public Moisin_Andrei_Lab2Context (DbContextOptions<Moisin_Andrei_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Moisin_Andrei_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Moisin_Andrei_Lab2.Models.Publisher> Publisher { get; set; }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Policy;
+using System.Xml.Linq;
+
+namespace Moisin_Andrei_Lab2.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        [Display(Name = "Book Title")]
+        public string Title { get; set; }
+        public string Author { get; set; }
+        [Column(TypeName = "decimal(6, 2)")]
+        public decimal Price { get; set; }
+
+        [DataType(DataType.Date)] 
+        public DateTime PublishingDate { get; set; }
+
+        public int? PublisherID { get; set; }
+        public Publisher? Publisher { get; set; }
+    }
+}
