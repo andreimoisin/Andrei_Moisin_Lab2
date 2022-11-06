631fd08d97489b2541c5c7711adf52e298a577e1
andreimoisin <98484063+andreimoisin@users.noreply.github.com>
2022-11-06 18:27:08 +0200

    Rezolvare lab 4

---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Moisin_Andrei_Lab2.Models.ViewModels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
--- Pages/Books/Index.cshtml.cs
+++ Pages/Books/Index.cshtml.cs
@@ -19,14 +19,24 @@ namespace Moisin_Andrei_Lab2.Pages.Books
             _context = context;
         }
 
-        public IList<Book> Book { get;set; } = default!;
+        public IList<Book> Book { get; set; } = default!;
         public BookData BookD { get; set; }
         public int BookID { get; set; }
         public int CategoryID { get; set; }
 
-        public async Task OnGetAsync(int? id, int? categoryID)
+        public string TitleSort { get; set; }
+        public string AuthorSort { get; set; }
+
+        public string CurrentFilter { get; set; }
+
+        public async Task OnGetAsync(int? id, int? categoryID, string sortOrder, string searchString)
         {
             BookD = new BookData();
+
+            TitleSort = String.IsNullOrEmpty(sortOrder) ? "title_desc" : "";
+            AuthorSort = String.IsNullOrEmpty(sortOrder) ? "author_desc" : "";
+
+            CurrentFilter = searchString;
 
             BookD.Books = await _context.Book
                 .Include(b => b.Author)
@@ -37,12 +47,31 @@ namespace Moisin_Andrei_Lab2.Pages.Books
                 .OrderBy(b => b.Title)
                 .ToListAsync();
 
-            if (_context.Book != null)
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                BookD.Books = BookD.Books.Where(s => s.Author.FirstName.Contains(searchString)
+                || s.Author.LastName.Contains(searchString)
+                || s.Title.Contains(searchString));
+            }
+
+                if (_context.Book != null)
             {
                 Book = await _context.Book
                     .Include(b => b.Publisher)
                     .Include(b => b.Author)
                     .ToListAsync();
+            }
+
+            switch (sortOrder)
+            {
+                case "title_desc": 
+                    BookD.Books = BookD.Books
+                        .OrderByDescending(s => s.Title);
+                    break;
+                case "author_desc": 
+                    BookD.Books = BookD.Books
+                        .OrderByDescending(s => s.Author.FullName); 
+                    break;
             }
         }
     }
